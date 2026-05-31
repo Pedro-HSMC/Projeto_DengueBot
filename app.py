6294c4061208f0a6e11a70a43285d951b27a981d
Pedro-HSMC <phsmc.16@gmail.com>
2026-05-31 14:26:11 -0300

    requisitos render

--- app.py
+++ app.py
@@ -90,5 +90,8 @@ def chat():
     # Envia tudo pronto para o HTML
     return render_template('index.html', historico=session['historico'], opcoes=opcoes)
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+import os
+
+if __name__ == "_main_":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
